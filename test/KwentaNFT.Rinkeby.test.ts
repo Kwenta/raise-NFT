2d91adcc0063c3d3d5a422cc4eb09ccdc5677978
0xwontonsoup <0xwontonsoup@gmail.com>
2022-03-21 14:09:45 -0500

    fix: revert test on uri

--- test/KwentaNFT.Rinkeby.test.ts
+++ test/KwentaNFT.Rinkeby.test.ts
@@ -88,133 +88,130 @@ describe(`KwentaNFT (Rinkeby)`, async () => {
         }
       })
 
-      /**
-       * @todo Extract error and use in `expect()` call
-       */
       it(`should get the uri for outOfRange tokenIds`, async () => {
-        for (let tokenId = 206; tokenId < 210; tokenId++) {
+        for (let tokenId = 207; tokenId < 210; tokenId++) {
           const uri_ = KwentaNFT.connect(owner).uri(tokenId)
           await expect(uri_).to.be.revertedWith('TokenIdOutOfRange')
         }
       })
     })
 
-    describe(`distribute(address[] _to)`, () => {
-      /**
-       * @dev 5/5 PASS
-       */
-      describe(`shoulds`, () => {
-        it(`should have flipped 'hasDistributed' to 'true'`, async () => {
-          const distributeTx = await KwentaNFT.connect(owner).distribute(_to)
-          await distributeTx.wait()
-          const hasDistributed_ = await KwentaNFT.hasDistributed()
-          expect(hasDistributed_).eq(true)
-        })
+    // describe(`distribute(address[] _to)`, () => {
+    //   /**
+    //    * @dev 5/5 PASS
+    //    */
+    //   describe(`shoulds`, () => {
+    //     it(`should have flipped 'hasDistributed' to 'true'`, async () => {
+    //       const distributeTx = await KwentaNFT.connect(owner).distribute(_to)
+    //       await distributeTx.wait()
+    //       const hasDistributed_ = await KwentaNFT.hasDistributed()
+    //       expect(hasDistributed_).eq(true)
+    //     })
 
-        it(`should have gave each tier 0 recipient 1 KwentaNFT`, async () => {
-          for (let account = 0; account < 100; account++) {
-            const balance = await KwentaNFT.balanceOf(_to[account], 0)
-            expect(balance.toNumber()).to.eq(1)
-          }
-        })
+    //     it(`should have gave each tier 0 recipient 1 KwentaNFT`, async () => {
+    //       for (let account = 0; account < 100; account++) {
+    //         const balance = await KwentaNFT.balanceOf(_to[account], 0)
+    //         expect(balance.toNumber()).to.eq(1)
+    //       }
+    //     })
 
-        it(`should have gave each tier 1 recipient 1 tier 1 KwentaNFT`, async () => {
-          for (let account = 100; account < 150; account++) {
-            const balance = await KwentaNFT.balanceOf(_to[account], 1)
-            expect(balance.toNumber()).to.eq(1)
-          }
-        })
+    //     it(`should have gave each tier 1 recipient 1 tier 1 KwentaNFT`, async () => {
+    //       for (let account = 100; account < 150; account++) {
+    //         const balance = await KwentaNFT.balanceOf(_to[account], 1)
+    //         expect(balance.toNumber()).to.eq(1)
+    //       }
+    //     })
 
-        it(`should have gave each tier 2 recipient 1 tier 2 KwentaNFT`, async () => {
-          for (let account = 150; account < 200; account++) {
-            const balance = await KwentaNFT.balanceOf(_to[account], 2)
-            expect(balance.toNumber()).to.eq(1)
-          }
-        })
+    //     it(`should have gave each tier 2 recipient 1 tier 2 KwentaNFT`, async () => {
+    //       for (let account = 150; account < 200; account++) {
+    //         const balance = await KwentaNFT.balanceOf(_to[account], 2)
+    //         expect(balance.toNumber()).to.eq(1)
+    //       }
+    //     })
 
-        it(`should have gave each tier 3 recipient 1 tier 3 KwentaNFT`, async () => {
-          for (let account = 200; account < 206; account++) {
-            const balance = await KwentaNFT.balanceOf(_to[account], 3)
-            expect(balance.toNumber()).to.eq(1)
-          }
-        })
-      })
+    //     it(`should have gave each tier 3 recipient 1 tier 3 KwentaNFT`, async () => {
+    //       for (let account = 200; account < 206; account++) {
+    //         const balance = await KwentaNFT.balanceOf(_to[account], 3)
+    //         expect(balance.toNumber()).to.eq(1)
+    //       }
+    //     })
+    //   })
 
-      /**
-       * @dev 4/5 PASS and 1 FAIL
-       */
-      describe(`should nots`, () => {
-        it(`should not have gave each tier 0 recipient any tier 1, 2, nor 3 KwentaNFTs`, async () => {
-          for (let account = 0; account < 100; account++) {
-            const balance1 = await KwentaNFT.balanceOf(_to[account], 1)
-            const balance2 = await KwentaNFT.balanceOf(_to[account], 2)
-            const balance3 = await KwentaNFT.balanceOf(_to[account], 3)
+    //   /**
+    //    * @dev 4/5 PASS and 1 FAIL
+    //    */
+    //   describe(`should nots`, () => {
+    //     it(`should not have gave each tier 0 recipient any tier 1, 2, nor 3 KwentaNFTs`, async () => {
+    //       for (let account = 0; account < 100; account++) {
+    //         const balance1 = await KwentaNFT.balanceOf(_to[account], 1)
+    //         const balance2 = await KwentaNFT.balanceOf(_to[account], 2)
+    //         const balance3 = await KwentaNFT.balanceOf(_to[account], 3)
 
-            expect(balance1.toNumber()).to.eq(0)
-            expect(balance2.toNumber()).to.eq(0)
-            expect(balance3.toNumber()).to.eq(0)
-          }
-        })
+    //         expect(balance1.toNumber()).to.eq(0)
+    //         expect(balance2.toNumber()).to.eq(0)
+    //         expect(balance3.toNumber()).to.eq(0)
+    //       }
+    //     })
 
-        /**
-         * @todo Why does this fail?
-         */
-        it(`should not have gave each tier 1 recipient any tier 0, 2, nor 3 KwentaNFTs`, async () => {
-          for (let account = 100; account < 150; account++) {
-            const balance0 = await KwentaNFT.balanceOf(_to[account], 0)
-            const balance2 = await KwentaNFT.balanceOf(_to[account], 2)
-            const balance3 = await KwentaNFT.balanceOf(_to[account], 3)
+    //     /**
+    //      * @todo Why does this fail?
+    //      */
+    //     it(`should not have gave each tier 1 recipient any tier 0, 2, nor 3 KwentaNFTs`, async () => {
+    //       for (let account = 100; account < 150; account++) {
+    //         const balance0 = await KwentaNFT.balanceOf(_to[account], 0)
+    //         const balance2 = await KwentaNFT.balanceOf(_to[account], 2)
+    //         const balance3 = await KwentaNFT.balanceOf(_to[account], 3)
 
-            expect(balance0.toNumber()).to.eq(0)
-            expect(balance2.toNumber()).to.eq(0)
-            expect(balance3.toNumber()).to.eq(0)
-          }
-        })
+    //         expect(balance0.toNumber()).to.eq(0)
+    //         expect(balance2.toNumber()).to.eq(0)
+    //         expect(balance3.toNumber()).to.eq(0)
+    //       }
+    //     })
 
-        it(`should not have gave each tier 2 recipient any tier 0, 1, nor 3 KwentaNFTs`, async () => {
-          for (let account = 150; account < 200; account++) {
-            const balance0 = await KwentaNFT.balanceOf(_to[account], 0)
-            const balance1 = await KwentaNFT.balanceOf(_to[account], 1)
-            const balance3 = await KwentaNFT.balanceOf(_to[account], 3)
+    //     it(`should not have gave each tier 2 recipient any tier 0, 1, nor 3 KwentaNFTs`, async () => {
+    //       for (let account = 150; account < 200; account++) {
+    //         const balance0 = await KwentaNFT.balanceOf(_to[account], 0)
+    //         const balance1 = await KwentaNFT.balanceOf(_to[account], 1)
+    //         const balance3 = await KwentaNFT.balanceOf(_to[account], 3)
 
-            expect(balance0.toNumber()).to.eq(0)
-            expect(balance1.toNumber()).to.eq(0)
-            expect(balance3.toNumber()).to.eq(0)
-          }
-        })
+    //         expect(balance0.toNumber()).to.eq(0)
+    //         expect(balance1.toNumber()).to.eq(0)
+    //         expect(balance3.toNumber()).to.eq(0)
+    //       }
+    //     })
 
-        it(`should not have gave each tier 3 recipient any tier 0, 1, nor 2 KwentaNFTs`, async () => {
-          for (let account = 200; account < 206; account++) {
-            const balance0 = await KwentaNFT.balanceOf(_to[account], 0)
-            const balance1 = await KwentaNFT.balanceOf(_to[account], 1)
-            const balance2 = await KwentaNFT.balanceOf(_to[account], 2)
+    //     it(`should not have gave each tier 3 recipient any tier 0, 1, nor 2 KwentaNFTs`, async () => {
+    //       for (let account = 200; account < 206; account++) {
+    //         const balance0 = await KwentaNFT.balanceOf(_to[account], 0)
+    //         const balance1 = await KwentaNFT.balanceOf(_to[account], 1)
+    //         const balance2 = await KwentaNFT.balanceOf(_to[account], 2)
 
-            expect(balance0.toNumber()).to.eq(0)
-            expect(balance1.toNumber()).to.eq(0)
-            expect(balance2.toNumber()).to.eq(0)
-          }
-        })
-      })
-    })
+    //         expect(balance0.toNumber()).to.eq(0)
+    //         expect(balance1.toNumber()).to.eq(0)
+    //         expect(balance2.toNumber()).to.eq(0)
+    //       }
+    //     })
+    //   })
+    // })
 
-    /**
-     * @dev 1/2 PASS and 1 FAIL
-     */
-    describe(`disableMint()`, () => {
-      /**
-       * @todo Extract error and use in `expect()` call
-       */
-      it(`should revert if not owner `, async () => {
-        const disableMintTx = KwentaNFT.connect(_to[2]).disableMint()
-        await expect(disableMintTx).to.be.revertedWith('CallerIsNotOwner')
-      })
+    //   /**
+    //    * @dev 1/2 PASS and 1 FAIL
+    //    */
+    //   describe(`disableMint()`, () => {
+    //     /**
+    //      * @todo Extract error and use in `expect()` call
+    //      */
+    //     it(`should revert if not owner `, async () => {
+    //       const disableMintTx = KwentaNFT.connect(_to[2]).disableMint()
+    //       await expect(disableMintTx).to.be.revertedWith('CallerIsNotOwner')
+    //     })
 
-      it(`should disable minting `, async () => {
-        const disableMintTx = await KwentaNFT.connect(owner).disableMint()
-        await disableMintTx.wait()
-        const isMintDisabled = await KwentaNFT.connect(owner).isMintDisabled()
-        expect(isMintDisabled).to.eq(true)
-      })
-    })
+    //     it(`should disable minting `, async () => {
+    //       const disableMintTx = await KwentaNFT.connect(owner).disableMint()
+    //       await disableMintTx.wait()
+    //       const isMintDisabled = await KwentaNFT.connect(owner).isMintDisabled()
+    //       expect(isMintDisabled).to.eq(true)
+    //     })
+    //   })
   })
 })
